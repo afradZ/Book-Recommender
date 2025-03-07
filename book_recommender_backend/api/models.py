1607e4f4f7e31709aa00b762a97928df969b195d
afradZ <afradzakaria@gmail.com>
2025-03-07 23:49:39 +0000

    initlal setup

---
+++ book_recommender_backend/api/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    description = models.TextField()
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    rating = models.IntegerField()
